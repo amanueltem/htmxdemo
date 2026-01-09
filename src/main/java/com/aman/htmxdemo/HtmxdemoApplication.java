67c18793ce792c353e9a0e7f688deaf31b165292
amanueltem <amanueltemesgen62@gmail.com>
2026-01-09 15:35:02 +0300

    postgres database configured.

--- src/main/java/com/aman/htmxdemo/HtmxdemoApplication.java
+++ src/main/java/com/aman/htmxdemo/HtmxdemoApplication.java
@@ -1,13 +1,20 @@
 package com.aman.htmxdemo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class HtmxdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HtmxdemoApplication.class, args);
+ static void main(String[] args) {
+     Dotenv dotenv = Dotenv.load();
+     System.setProperty("HTMX_DB_URL", Objects.requireNonNull(dotenv.get("HTMX_DB_URL")));
+     System.setProperty("HTMX_DB_USERNAME", Objects.requireNonNull(dotenv.get("HTMX_DB_USERNAME")));
+     System.setProperty("HTMX_DB_PASSWORD", Objects.requireNonNull(dotenv.get("HTMX_DB_PASSWORD")));
+        SpringApplication.run(HtmxdemoApplication.class, args);
 	}
 
 }
--- src/main/java/com/aman/htmxdemo/TaskController.java
+++ src/main/java/com/aman/htmxdemo/TaskController.java
@@ -35,3 +35,4 @@ public class TaskController {
         return "";
     }
 }
+//vAeHKrBGBHJo1adq
